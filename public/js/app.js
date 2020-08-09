361594b60ae358916b03ae4261a47f1bd3d1ea40
plasmatech8 <com0004@gmail.com>
2020-08-09 16:03:16 +1000

    Initialised HTML template

---
+++ public/js/app.js
@@ -0,0 +1,15 @@
+const requestModal = document.querySelector('.new-request');
+const requestLink = document.querySelector('.add-request');
+
+// open request modal
+requestLink.addEventListener('click', () => {
+  requestModal.classList.add('open');
+});
+
+// close request modal
+requestModal.addEventListener('click', (e) => {
+  // If clicked on modal background, but not on the dialog box, close
+  if (e.target.classList.contains('new-request')) {
+    requestModal.classList.remove('open');
+  }
+});
